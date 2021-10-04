0b18e3fffb6ecd78a76bc1491bed8920a22ecb7b
adaldin <aledaldin@gmail.com>
2021-10-04 18:02:23 +0200

    uploading js practice+ old css

---
+++ students/ale-daldin/JS/variables/intro.js
@@ -0,0 +1,44 @@
+/*1.-Solicitar al usuario dos variables numéricas (usando prompt) y realizar su suma, resta multiplicación y division. Mostrar los resultados en pantalla, maquetando con flexbox debemos ver la operación ( los numeros y el operador ) y seguidos de un símbolo '=' el resultado.*/
+// String
+let operandOne = prompt('Introduce un numero');
+let operandTwo=prompt('Introduce otro numero');
+// // Conversión
+let convertOne= parseFloat(operandOne);
+let convertTwo =parseFloat(operandTwo);
+// // operaciones
+// let sumResult = convertOne+convertTwo;
+// let restResult= convertOne-convertTwo;
+// let multResult= convertOne*convertTwo;
+// let divResult= convertOne/convertTwo;
+// // Impresión en pantalla
+// document.write(`<div class="operation__container">  <div class="operands__container">La suma de ${convertOne} y ${convertTwo} es: </div> <div class="result__container">${sumResult}</div></div>`);
+// document.write(`<div class="operation__container">  <div class="operands__container">La resta de ${convertOne} y ${convertTwo} es: </div> <div class="result__container">${restResult}</div></div>`);
+// document.write(`<div class="operation__container">  <div class="operands__container">La multiplicación de ${convertOne} y ${convertTwo} es: </div> <div class="result__container">${multResult}</div></div>`);
+// document.write(`<div class="operation__container">  <div class="operands__container">La división de ${convertOne} y ${convertTwo} es: </div> <div class="result__container">${divResult}</div></div>`);
+
+// -------------
+
+// 2.-Solicitar al usuario dos variables de tipo string y mostrarlas concatenadas por pantalla.
+// // Operandos
+// let strOne = prompt('Introduce una palabra');
+// let strTwo=prompt('Introduce otra palabra');
+// // Concat
+// let concatResult = strOne+strTwo;
+// // Impresión en pantalla
+// document.write(`<div class="concat__container">  <div class="str__container">Tu primera palabra ${strOne} y  tu segunda palabra${strTwo} forman: </div> <div class="result__container">${concatResult}</div></div>`);
+
+
+// -------------
+
+// 3.-Ejercicio que pida al usuario los grados Celsius (X) e imprima por pantalla sus correspondientes grados Farenheit del siguiente modo "X ºC son YºF"
+// let celsius = prompt('Introduce un numero en la escala Celsius para convertir a Farenheit');
+// let farenheit = (celsius*1.8000) + 32;
+// document.write(`<div class="celsius__container"> ${celsius}ºC son ${farenheit}ºF`);
+// -------------
+
+// 4.-Imprimir la longitud del string resultante del ejercicio 2. (Este requiere investigación).
+// let contactLength =concatResult.length;
+// document.write(`<div class="concat__container--length"> Tu cantidad de caracteres en ${concatResult} es :${contactLength}`);
+
+// 5.- Volver al ejercicio 1 y mostrar por consola el tipo de datos de la variable numerica solicitada por prompt antes de ser parseada y después del parseo.
+console.log('Antes del pareseo los valores ingresados son' + ' ' + typeof(operandOne)+ ' ' + typeof(operandTwo) + ' ' + 'y luego del pareseo'+ ' ' + typeof(convertOne) + ' ' + typeof(convertTwo));
