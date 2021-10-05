49f9daf50ff9cbeeeb8fa763b277a79166da52b8
manu-grande-lope <manugrande@gmail.com>
2021-10-05 14:58:17 +0200

    creación de carpetas, archivos y branch

---
+++ students/manu-lope/Ejercicio-de-condicionales/script.js
@@ -0,0 +1,5 @@
+let airQuality = prompt('Introduce la calidad del aire que marca tu dispositivo.')
+let contaminacion = parseFloat(airQuality)
+if(contaminacion > 65 {
+document.write (`<div><p>Velocidad permitida hoy 70km/h</p></div>)
+}else{}
