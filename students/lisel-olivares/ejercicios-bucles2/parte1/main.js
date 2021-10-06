cf90f76278d6e706c8f200c1842a2dc2732bce5e
LAPTOP-6OR99Q7A\lisel <lisel.olivares@gmail.com>
2021-10-06 16:30:28 +0200

    First exercise of conditionals, wednesday

---
+++ students/lisel-olivares/ejercicios-bucles2/parte1/main.js
@@ -0,0 +1,8 @@
+const n1 = prompt ('Introcude un numero');
+const n2 = prompt ('Introduce otro numero');
+
+if (n1 > n2){
+    document.write (`<h1>${n1}</h1>`);
+}else if(n2 > n1){
+    document.write (`<h1>${n2}</h1>`);
+}
