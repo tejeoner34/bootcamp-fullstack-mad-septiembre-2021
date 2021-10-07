3719b152b39118fd0f2573691591513ee03491b5
Axel Brigo <axelbrigo1993@gmail.com>
2021-10-07 10:44:14 +0200

    Ejercicios del dia 06/10/2021

---
+++ students/axel-brigo/tarea-javascript/06-10-2021 EJERCICIOS/EJERCICIO-5/funcionalidad.js
@@ -0,0 +1,15 @@
+let asterisco = "*";
+
+for (let i = 1; i <=5 ; i++) {
+    let imprimir = asterisco.repeat(i)
+    document.write(`<p>${imprimir}</p>`)
+
+    
+    }
+
+let asterisco2 = "*";
+
+for (let f = 5; f >=0; f--){
+    let imprimir2 = asterisco2.repeat(f)
+    document.write(`<p>${imprimir2}</p>`)
+}
