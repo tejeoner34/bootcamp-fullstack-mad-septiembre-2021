fca52abe490a1360c66378cf3569aff6284635ec
Diego Guzmán Medrano <guzmanmedrano.diego@gmail.com>
2021-10-07 10:38:24 +0200

    Added Alex's solution to exercise

--- students/diego-guzman/js-practice/js-booleans/index.js
+++ students/diego-guzman/js-practice/js-booleans/index.js
@@ -19,3 +19,28 @@ if(count>=8){
 else{
     document.write(`<h1>No se están cumpliendo los servicios mínimos</h1>`)
 }
+
+// Explicación Alex
+
+const TRAIN_COUNT = 10;
+const trainDeparture = [];
+
+for(let i = 0; i<TRAIN_COUNT; i++){
+    const userInput = prompt ('Ha salido el siguiente tren? (true/false)');
+    const userInputParsed = userInput.toLowerCase()=== 'true';
+    trainDeparture[i] = userInputParsed;
+}
+//aqui ya tendría un array de booleanos
+
+let trainCountDeparture = 0; // variable para saber cuantos trenes han salido
+for(let i=0; i<trainDeparture.length; i++){
+    if(trainDeparture[i]){
+        trainCountDeparture++;
+    }
+}
+//Aqui ya tengo la cantidad de trenes que hayan salido
+
+const PERCENT_COUNT_NEEDED = Math.ceil(75*TRAIN_COUNT/100); //Regla de tres redondeada siempre hacia arriba
+
+const OUTPUT_MINIMUN_SERVICES = trainCountDeparture >= PERCENT_COUNT_NEEDED ? '<h1>Se han cumplido los servicios mínimos</h1>' : '<h1 style="color:red">No se han cumplido los servicios mínimos</h1>';
+document.write(OUTPUT_MINIMUN_SERVICES);
