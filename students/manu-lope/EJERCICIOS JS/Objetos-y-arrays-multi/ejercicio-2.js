efaa17f3b20c149bc8a1d1a9b9c41a7c55075bdb
manu-grande-lope <manugrande@gmail.com>
2021-10-11 14:55:05 +0200

    ejercicio-1-objetos

---
+++ students/manu-lope/EJERCICIOS JS/Objetos-y-arrays-multi/ejercicio-2.js
@@ -0,0 +1,29 @@
+
+//  * 2- Crear un objeto que represente un coche y tenga las propiedades marca, modelo, matricula, velocidad actual y algo que me indique si está encendido o apagado
+
+// Los siguientes ejercicios dependen del anterior por lo que los haremos seguidos en el mismo documento:
+
+//  * 3- Crear un array de objetos coche
+
+//  * 4- Imprimir por pantalla la velocidad del 4º coche
+
+//  * 5- Hacer una funcion que devuelva un array con los coches de una marca que se le pasa por parametro
+
+
+//  * 6- Hacer una funcion que me indque si hay algun coche encendido
+
+//  * 7- Hacer una funcion que me devuelva la velocidad media de todos los coches
+
+// let coche = {
+//     marca: 'seat',
+//     modelo: 'león',
+//     matricula :'4567jjj',
+//     ruedas: 4,
+//     color: 'rojo',
+//     velocidad: 100,
+//     ITV: true,
+// };
+// document.write(`<p> 'Ha pasado el coche la ITV ' ${coche.ITV}</p>`);
+// coche.acelerar=(a,b) => (a+b);
+// let nuevaVelocidad = coche.acelerar(coche.velocidad, 7);
+// document.write(nuevaVelocidad);
