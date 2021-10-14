71472e7fa112f970ddfb171780ef6baf2b7c4df7
Diego Guzmán Medrano <guzmanmedrano.diego@gmail.com>
2021-10-14 12:50:22 +0200

    JS-DOM function card creator fixed

--- students/diego-guzman/js-practice/js-DOM/practice-exercises/app.js
+++ students/diego-guzman/js-practice/js-DOM/practice-exercises/app.js
@@ -57,82 +57,78 @@
 
 // 5- Generar una función que dado una ciudad, devuelva el elemento del DOM de la card para poder añadirlo al DOM de la página
 let myCityTwo = {
-    name: 'Oulu',
- 	description: 'Capital of Northern Scandinavia',
- 	country: 'Finland',
- 	urlImage: 'https://www.turismofinlandia.es/wp-content/uploads/2018/02/oulu-finlandia-escandinavia-1024x768.jpg',
+	name: 'Oulu',
+	description: 'Capital of Northern Scandinavia',
+	country: 'Finland',
+	urlImage: 'https://www.turismofinlandia.es/wp-content/uploads/2018/02/oulu-finlandia-escandinavia-1024x768.jpg',
 	coordinates: {
- 		latitude: 65.01236,
- 		longitude: 25.46816, 
-    }
+		latitude: 65.01236,
+		longitude: 25.46816,
+	}
 }
 
 let myCity = {
-    name: 'Vienna',
+	name: 'Vienna',
 	description: 'Capital of Austria',
 	country: 'Austria',
- 	urlImage: 'https://www.wien.info/resource/image/290578/3x2/832/561/a91e087927a65f46f9f779797068f401/Je/40367-graben-einkaufen-shopping-altstadt-einkaufsstrassen.jpg',
+	urlImage: 'https://www.wien.info/resource/image/290578/3x2/832/561/a91e087927a65f46f9f779797068f401/Je/40367-graben-einkaufen-shopping-altstadt-einkaufsstrassen.jpg',
 	coordinates: {
 		latitude: 48.208354,
-		longitude:  16.372504, 
-    }
+		longitude: 16.372504,
+	}
 }
-let myCityArray = [myCity,myCityTwo];
+let myCityArray = [myCity, myCityTwo];
 
-function cityCard (obj) {
+function cityCard(obj) {
 
-	for(let i=0; i<obj.length;i++){
+	for (let i = 0; i < obj.length; i++) {
 
-const firstDiv = document.createElement('div');
-firstDiv.classList.add('card__container');
-document.body.appendChild(firstDiv);
+		const firstDiv = document.createElement('div');
+		firstDiv.classList.add('card__container');
+		
 
-const cityImg = document.createElement('img');
-cityImg.classList.add('img__background');
-cityImg.src=obj[i].urlImage;
-document.querySelector('.card__container').appendChild(cityImg);
+		const cityImg = document.createElement('img');
+		cityImg.classList.add('img__background');
+		cityImg.src = obj[i].urlImage;
+		firstDiv.appendChild(cityImg);
 
-const cityName = document.createElement('h3');
-cityName.classList.add('city__name');
-cityName.textContent =obj[i].name;
-document.querySelector('.card__container').appendChild(cityName);
-
-
-const cityDescription = document.createElement('p');
-cityDescription.classList.add('city__description');
-cityDescription.textContent =obj[i].description;
-document.querySelector('.card__container').appendChild(cityDescription);
-
-const cityCountry = document.createElement('p');
-cityCountry.classList.add('city__country');
-cityCountry.textContent =obj[i].country;
-document.querySelector('.card__container').appendChild(cityCountry);
-
-const listContainer = document.querySelector('.ul__container');
-
-const coordinates = document.createElement('ul');
-coordinates.classList.add('ul__container');
-coordinates.textContent =obj[i].coordinates;
-document.querySelector('.card__container').appendChild(coordinates);
-
-const latituteCoordinate = document.createElement('li');
-latituteCoordinate.classList.add('li__item');
-latituteCoordinate.textContent =obj[i].coordinates.latitude;
-document.querySelector('.ul__container').appendChild(latituteCoordinate);
-
-const longitudeCoordinate = document.createElement('li');
-longitudeCoordinate.classList.add('li__item');
-longitudeCoordinate.textContent =obj[i].coordinates.latitude;
-document.querySelector('.ul__container').appendChild(longitudeCoordinate);
+		const cityName = document.createElement('h3');
+		cityName.classList.add('city__name');
+		cityName.textContent = obj[i].name;
+		firstDiv.appendChild(cityName);
 
 
-}
+		const cityDescription = document.createElement('p');
+		cityDescription.classList.add('city__description');
+		cityDescription.textContent = obj[i].description;
+		firstDiv.appendChild(cityDescription);
+
+		const cityCountry = document.createElement('p');
+		cityCountry.classList.add('city__country');
+		cityCountry.textContent = obj[i].country;
+		firstDiv.appendChild(cityCountry);
+
+		const coordinates = document.createElement('ul');
+		coordinates.classList.add('ul__container');
+		coordinates.textContent = obj[i].coordinates;
+		firstDiv.appendChild(coordinates);
+
+		const latituteCoordinate = document.createElement('li');
+		latituteCoordinate.classList.add('li__item');
+		latituteCoordinate.textContent = obj[i].coordinates.latitude;
+		coordinates.appendChild(latituteCoordinate);
+
+		const longitudeCoordinate = document.createElement('li');
+		longitudeCoordinate.classList.add('li__item');
+		longitudeCoordinate.textContent = obj[i].coordinates.latitude;
+		coordinates.appendChild(longitudeCoordinate);
+
+		document.body.appendChild(firstDiv);
+
+	}
 }
 
 cityCard(myCityArray);
-
-
-
 
 
 
